4f2d0f4e6c15011fa0716bd3d401019057d88b2e
ehabhamdy <ehabhamdy2012@gmail.com>
2021-07-18 10:56:20 +0200

    Server uses env variables

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const username = process.env.POSTGRES_USERNAME;
+
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
 
 app.get("/health", (req, res, next) => {
-  res.status(200).send("Hello!");
+  const s = "Hello " + username;
+  res.status(200).send(s);
 });
 
